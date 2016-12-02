37769d0a83609d23e7fbb71d8f21d96644ecbf95
Grégoire Noyelle <gregorioprimo@gmail.com>
2016-12-02 15:05:51 +0100

    Ajout du script slick slider en bas de la page home

--- lib/func.wordpress.php
+++ lib/func.wordpress.php
@@ -3,3 +3,18 @@
 
 //Enlever la barre Admin en front
 // show_admin_bar( false );
+
+
+add_action( 'wp_footer', 'slick_script', 9000 );
+function slick_script() {
+	// Si ce n'est pas l'accueil, le script s'arrête
+	if ( ! is_home() ) { return; }
+?>	
+
+	<script type="text/javascript">
+		jQuery('.slider-slick').slick({
+		  autoplay: true,
+		  autoplaySpeed: 2000,
+		});
+	</script>
+<?php }
