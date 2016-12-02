6f18d9e182f1d492e2e671b67798f73bc7b44486
Grégoire Noyelle <gregorioprimo@gmail.com>
2016-12-02 15:09:32 +0100

    Changement de tous les préfixes de function pour cette extension.

--- lib/func.enqueue.php
+++ lib/func.enqueue.php
@@ -8,19 +8,20 @@
 **************************/
 
 //* enqueue style front
-add_action('wp_enqueue_scripts','gn_styles_front', 99);
-function gn_styles_front() {
+add_action('wp_enqueue_scripts','slick_styles_front', 99);
+function slick_styles_front() {
 	wp_register_style('slick', plugins_url( 'css/slick.css', dirname(__FILE__) ), array(), '1.0', 'all' );
-	wp_register_style('slick-theme', plugins_url( 'css/slick-theme.css', dirname(__FILE__) ), array('slick'), '1.0', 'all' );
+	wp_register_style('slick-theme', plugins_url( 'css/slick-theme.css', dirname(__FILE__) ), array(), '1.0', 'all' );
 	if ( is_home() ) {
 		wp_enqueue_style('slick');
+		wp_enqueue_style('slick-theme');
 	}
 	
 }
 
 //* enqueue script front
-add_action('wp_enqueue_scripts','gn_scripts_front');
-function gn_scripts_front() {
+add_action('wp_enqueue_scripts','slick_scripts_front');
+function slick_scripts_front() {
 	wp_register_script('slick',plugins_url( 'js/slick.min.js', dirname(__FILE__) ), array('jquery'),'1.0',true);
 	if ( is_home() ) {
 		wp_enqueue_script('slick');
@@ -33,15 +34,15 @@ function gn_scripts_front() {
 **************************/
 
 //* enqueue style back
-// add_action('admin_enqueue_scripts','gn_styles_back', 99);
-function gn_styles_back() {
+// add_action('admin_enqueue_scripts','slick_styles_back', 99);
+function slick_styles_back() {
 	wp_register_style('styles-projet-back', plugins_url( 'css/styles-projet-back.css', dirname(__FILE__) ), array(), '1.0', 'all' );
 	wp_enqueue_style('styles-projet-back');
 }
 
 //* enqueue script back
-// add_action('admin_enqueue_scripts','gn_scripts_back');
-function gn_scripts_back() {
+// add_action('admin_enqueue_scripts','slick_scripts_back');
+function slick_scripts_back() {
 	wp_register_script('scripts-projet-back',plugins_url( 'js/scripts-projet-back.js', dirname(__FILE__) ), array('jquery'),'1.0',true);
 	wp_enqueue_script('scripts-projet-back');
 }
--- lib/func.genesis.php
+++ lib/func.genesis.php
@@ -2,8 +2,8 @@
 
 //* Fonction PHP du thème
 // Hook pour lancer les actions une fois que le thème est chargé
-add_action( 'after_setup_theme', 'gn_function_theme', 15 );
-function gn_function_theme() {
+add_action( 'after_setup_theme', 'slick_function_theme', 15 );
+function slick_function_theme() {
 	//* Vérifier que Genesis est actif
 	if( ! function_exists( 'genesis_get_option' ) ) {
 		return;
